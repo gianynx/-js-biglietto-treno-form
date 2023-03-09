3227ef36774b4edc8af951fe0db63acd7790d7c5
Gianmarco Incocciati <minusgian@gmail.com>
2023-03-09 16:08:15 +0100

    add function content / 'click'

--- js/script.js
+++ js/script.js
@@ -8,5 +8,19 @@ const kmInput = app.querySelector('input[name="km"]');
 
 const button = document.querySelector('#btn');
 button.addEventListener('click', function() {
-    
+    const kilometersNumber;
+    const passengerAge;
+    const centsKm = 0.21;
+    const ticketPrice = kilometersNumber * centsKm;
+    let totalPrice = ticketPrice;
+    const discount_20 = (ticketPrice * 20) / 100;
+    const discount_40 = (ticketPrice * 40) / 100;
+    if (passengerAge < 18) {
+        totalPrice = totalPrice - discount_20;
+    }
+    if (passengerAge >= 65) {
+        totalPrice = totalPrice - discount_40;
+    }
+    let messagge = 'Il prezzo totale del biglietto è di ';
+    document.getElementById("total_price").innerHTML = messagge + totalPrice.toFixed(2) + '€ !';
 });
