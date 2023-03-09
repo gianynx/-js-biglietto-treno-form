a87780d023795ee74f90581db53a47abf8d85dd9
Gianmarco Incocciati <minusgian@gmail.com>
2023-03-09 15:51:46 +0100

    add const button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 const app = document.getElementById('app');
 
 const ageInput = app.querySelector('input[name="age"]');
+// console.log(ageInput);
 
 const kmInput = app.querySelector('input[name="km"]');
+// console.log(kmInput);
+
+const button = document.querySelector('#btn');
+button.addEventListener('click', function() {
+    
+});
